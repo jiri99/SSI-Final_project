96942f046a24df17cd564d9388fdf20430dd5a98
FucsikovaTereza <75226017+FucsikovaTereza@users.noreply.github.com>
2023-01-13 00:00:28 +0100

    version to check

--- init_position.py
+++ init_position.py
@@ -4,4 +4,20 @@ Created on Thu Jan 12 22:52:13 2023
 
 @author: fucsi
 """
+import map as mp
+import random
 
+
+dim = 11
+map = mp.generate_map(dim)
+
+def generate_init_position(map):
+    x = 0 
+    y = 0
+    
+    while map[x,y] != 0: 
+        x = random.randint(0,dim-1)
+        y = random.randint(0,dim-1)  
+    return x, y
+
+x, y = generate_init_position(map)
